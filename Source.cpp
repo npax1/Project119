47018f44a2abe586a789ac513eac2f57b1697359
npax1 <prakh55555@gmail.com>
2023-02-14 19:12:20 +0200

    it isn`t funny

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
+
+void swap(int* num1, int* num2) {
+	int swapp;
+	swapp = *num1;
+	*num1 = *num2;
+	*num2 = swapp;
+}
+
 
 int main() {
-    std::cout << "Hello, world!";
-    return 0;
-}gbtrg
+	int num1, num2;
+	cout << "Enter 1 number: ";
+	cin >> num1;
+	cout << "Enter 2 number: ";
+	cin >> num2;
+
+	swap(&num1, &num2);
+	cout << "Res -> " << num1 << "\t" << num2;
+}
