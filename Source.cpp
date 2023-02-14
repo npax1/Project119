b6f9943ffc75707d5ec5f618bcd531d046334969
npax1 <prakh55555@gmail.com>
2023-02-14 18:58:51 +0200

    add

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, world!";
-    return 0;
+	int num1;
+	cout << "Enter 1 number -> ";
+	cin >> num1;
+	int* num_1;
+	num_1 = &num1;
+	cout << "Result = " << num_1;
+
+	int num2;
+	cout << "\nEnter 2 number -> ";
+	cin >> num2;
+	int* num_2;
+	num_2 = &num2;
+	cout << "Result = " << num_2;
+	if (*num_1 > *num_2)
+	{
+		cout << '\n' << num1 << " > " << num2;
+	}
+	else if (*num_1 < *num_2)
+	{
+		cout << '\n' << num1 << " < " << num2;
+	}
+	else
+	{
+		cout << '\n' << num1 << " = " << num2;
+	}
 }
